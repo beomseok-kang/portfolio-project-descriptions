24cfad58f0c17bfd42716a4a74b7745941b54656
bumsuk980119 <beom.kang17@gmail.com>
2020-07-27 21:26:09 +0900

    Updates

---
+++ precache-manifest.772c3b71f8a721e799d989b4d2449e83.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74eb47aa283e9f30a0228ee9f8f45b5e",
+    "url": "/portfolio-project-descriptions/index.html"
+  },
+  {
+    "revision": "58b216f3aaa85dec2155",
+    "url": "/portfolio-project-descriptions/static/css/main.8ad55c23.chunk.css"
+  },
+  {
+    "revision": "4fb04b38118676c6e29a",
+    "url": "/portfolio-project-descriptions/static/js/2.7ebb5ad9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-project-descriptions/static/js/2.7ebb5ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b216f3aaa85dec2155",
+    "url": "/portfolio-project-descriptions/static/js/main.fb9e0dd4.chunk.js"
+  },
+  {
+    "revision": "bda4469636d796260e0f",
+    "url": "/portfolio-project-descriptions/static/js/runtime-main.0be5ed91.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/portfolio-project-descriptions/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/portfolio-project-descriptions/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/portfolio-project-descriptions/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "dec5355d3b5f81c3adc03b281760da95",
+    "url": "/portfolio-project-descriptions/static/media/b.dec5355d.png"
+  },
+  {
+    "revision": "4ba6ee80c7b9270e34bfae0186657f61",
+    "url": "/portfolio-project-descriptions/static/media/css3_logo.4ba6ee80.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/portfolio-project-descriptions/static/media/html5_logo.f2282f94.png"
+  },
+  {
+    "revision": "0b55eec222396a20dd47ee1b9d27d80f",
+    "url": "/portfolio-project-descriptions/static/media/nr.0b55eec2.png"
+  },
+  {
+    "revision": "767deafae537d175a75c2e46a64592b0",
+    "url": "/portfolio-project-descriptions/static/media/p.767deafa.png"
+  },
+  {
+    "revision": "a7212ed4c02ca28a98c310a947c21998",
+    "url": "/portfolio-project-descriptions/static/media/react_logo.a7212ed4.png"
+  },
+  {
+    "revision": "62cc7d7a7701fc5c78e8bfdfe1c58ad7",
+    "url": "/portfolio-project-descriptions/static/media/redux_logo.62cc7d7a.png"
+  },
+  {
+    "revision": "99e95eb0832309299b9f64b356ecd1f5",
+    "url": "/portfolio-project-descriptions/static/media/sass_logo.99e95eb0.png"
+  },
+  {
+    "revision": "3745d5de0728ec47786eb328c7076356",
+    "url": "/portfolio-project-descriptions/static/media/styledcomponents_logo.3745d5de.png"
+  },
+  {
+    "revision": "6f957bb8376fc400f67a77b6801afa5d",
+    "url": "/portfolio-project-descriptions/static/media/swh.6f957bb8.png"
+  },
+  {
+    "revision": "4e96f1110cb872407e837d4fc1440f96",
+    "url": "/portfolio-project-descriptions/static/media/typescript_logo.4e96f111.png"
+  },
+  {
+    "revision": "a78c8d8d937fe94c567b46dbf232b017",
+    "url": "/portfolio-project-descriptions/static/media/uae.a78c8d8d.png"
+  },
+  {
+    "revision": "7d7c8178d39066620918fecc6c9cb96e",
+    "url": "/portfolio-project-descriptions/static/media/wlt.7d7c8178.png"
+  }
+]);
